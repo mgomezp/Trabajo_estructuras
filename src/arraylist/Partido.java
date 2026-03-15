57cb7cfc3f07d3f6658f3f8fa93fe3c5c4636b23
Miguel Angel Gomez <m.gomezp@unimayor.edu.co>
2026-03-15 14:16:49 -0500

    Segundo commit: añado la clase partido en el paquete de arraylist

--- src/arraylist/Partido.java
+++ src/arraylist/Partido.java
@@ -1,5 +1,50 @@
 package arraylist;
 
 public class Partido {
+    private String local;
+    private String visitante;
+    private int golesL;
+    private int golesV;
+
+    public Partido(String data){
+        String[] p = data.split("::");
+        this.local = p[0];
+        this.visitante = p[1];
+        this.golesL = Integer.parseInt(p[2]);
+        this.golesV = Integer.parseInt(p[3]);
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public String getVisitante() {
+        return visitante;
+    }
+
+    public int getGolesL() {
+        return golesL;
+    }
+
+    public int getGolesV() {
+        return golesV;
+    }
+    public boolean esEmpate() {
+        return golesL == golesV;
+    }
+    public boolean ganoLocal() {
+        return golesL > golesV;
+    }
+    public boolean ganoVisitante() {
+        return golesV > golesL;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s %d - %d %-15s", local, golesL, golesV, visitante);
+    }
+
+    
+    
 
 }
